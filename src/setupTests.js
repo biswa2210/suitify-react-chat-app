417851d8e2935606d9faa947f46f780c2d9be7de
Biswarup Bhattacharjee <74323889+biswa2210@users.noreply.github.com>
2021-05-22 19:41:55 +0530

    Update setupTests.js

--- src/setupTests.js
+++ src/setupTests.js
@@ -3,3 +3,8 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+/*
+CREATED BY BISWARUP BHATTACHARJEE
+EMAIL    : bbiswa471@gmail.com
+PHONE NO : 6290272740
+*/
