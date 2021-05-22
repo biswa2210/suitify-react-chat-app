0d65cce73051821dec134df05928e427f20e1ad9
Biswarup Bhattacharjee <74323889+biswa2210@users.noreply.github.com>
2021-05-22 19:32:46 +0530

    Update joinUsernames.js

--- src/helpers/joinUsernames.js
+++ src/helpers/joinUsernames.js
@@ -2,7 +2,11 @@
 // usernames with commas. Also filters out current
 // user's username. If there is only one username
 // it just returns it.
-
+/*
+CREATED BY BISWARUP BHATTACHARJEE
+EMAIL    : bbiswa471@gmail.com
+PHONE NO : 6290272740
+*/
 export const joinUsernames = (people, currentUsername) => {
   return '@' + people
     .map(p => p.person.username)
