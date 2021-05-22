2c8143931a538799f0145c06dc45090cdc66e0bb
Biswarup Bhattacharjee <74323889+biswa2210@users.noreply.github.com>
2021-05-22 19:41:13 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { App } from 'components';
 import { BrowserRouter as Router } from 'react-router-dom';
-
+/*
+CREATED BY BISWARUP BHATTACHARJEE
+EMAIL    : bbiswa471@gmail.com
+PHONE NO : 6290272740
+*/
 ReactDOM.render(
   <React.StrictMode>
     <Router>
